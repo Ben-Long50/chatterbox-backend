2d06395cdbcfcaf35be0f2f52d8776b37243a153
Ben-Long50 <benjlong50@gmail.com>
2024-11-25 11:40:39 -0800

    added server status endpoint

--- routes/authentication.js
+++ routes/authentication.js
@@ -4,6 +4,10 @@ import userController from '../controllers/userController.js';
 
 const router = express.Router();
 
+router.get('/status', (req, res) =>
+  res.status(200).json({ message: 'Service is running' }),
+);
+
 router.post('/users/signup', userController.createUser);
 
 router.post('/users/signin', authController.userSignin);
